dea52c30f357d6a9c163b67b7d5447b60007c1ab
rdwilliams93 <r.d.williams1993@gmail.com>
2014-07-09 16:37:55 +0100

    frontend and json parser

--- hostparser.js
+++ hostparser.js
@@ -1,14 +1,14 @@
-  $.ajax({
+$.ajax({
 
-    url: "./fake-data.js",
+    url: "heroku-4loop.herokuapp.com" /*"this is the Heroku url"*/,
 
     dataType: "jsonp",
 
     success: function(data) {
 
-      $.each(data.response.results, function () {
+      $.each(/*relevant address when can see data*/, function () {
 
-        $( "#images" ).append("<li>" + '<img src="' + this['url'] + '">' + "</li>");
+        $( "#images" ).append("<li>" + '<img src="' +this['relevant name'] + '">' + "</li>");
 
       });
 
