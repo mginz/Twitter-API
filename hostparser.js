b0a87efae2f226c74e3353556e3fc2c04b5c997f
rdwilliams93 <r.d.williams1993@gmail.com>
2014-07-10 17:53:26 +0100

    changes

--- hostparser.js
+++ hostparser.js
@@ -1,15 +1,14 @@
 $.ajax({
 
-    //url: "heroku-4loop.herokuapp.com" /*"this is the Heroku url"*/,
-	url:"http://content.guardianapis.com/search?section=news&show-fields=trailText%2Cthumbnail",
+   url:"http://obscure-ridge-8042.herokuapp.com/",
    
    dataType: "jsonp",
 
     success: function(data) {
 
-      $.each(data.response.results/*relevant address when can see data*/, function () {
+      $.each(data.statuses, function () {
 
-        $( "#images" ).append("<li>" + '<img src="' +this['fields'].thumbnail + '">' + "</li>");
+        $( "#images" ).append("<li>" + '<img src="' +this.img + '">' + "</li>");
 
       });
 
--- indexsecure.js
+++ indexsecure.js
@@ -1,15 +1,12 @@
-	var http = require("http");
-	var Twit = require('twit')
-	var keystore = require ('./keystore.js')
+	var http = require("http"),
+	    Twit = require('twit'),
+	    keystore = require ('./keystore.js');
 
 function piri (){
 	var T = new Twit (keys);
 	T.get('search/tweets', { q: '#foundersandcoders since:2014-01-01', count:   10} , function(err, data, response) {
 		
 		console.log(data.statuses[0].entities.media[0].media_url);
-
-
-
 	});
 }
 
